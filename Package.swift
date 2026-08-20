2c04698b758a91be74a159327b9f2615ebac963c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:07:50 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,27 +1,39 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-memory-sequence-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
             name: "Memory Sequence Primitives",
             targets: ["Memory Sequence Primitives"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-memory-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-span-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-memory-cursor-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-sequence-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-memory-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-span-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-memory-cursor-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-sequence-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -29,8 +41,14 @@ let package = Package(
             dependencies: [
                 .product(name: "Memory Primitive", package: "swift-memory-primitives"),
                 .product(name: "Span Protocol Primitives", package: "swift-span-primitives"),
-                .product(name: "Memory Cursor Primitives", package: "swift-memory-cursor-primitives"),
-                .product(name: "Sequence Protocol Primitives", package: "swift-sequence-primitives"),
+                .product(
+                    name: "Memory Cursor Primitives",
+                    package: "swift-memory-cursor-primitives"
+                ),
+                .product(
+                    name: "Sequence Protocol Primitives",
+                    package: "swift-sequence-primitives"
+                ),
             ]
         ),
         .testTarget(
